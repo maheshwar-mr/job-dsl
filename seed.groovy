23eff75c090f05469fe72e450cf223f6672fddc0
maheshwar807 <maheshwar.mannur@gmail.com>
2020-07-19 11:12:23 +0530

    edited seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,8 +1,12 @@
 pipelineJob('example') {
+def repo = 'https://github.com/maheshwar807/sample_job.git'
     definition {
         cpsScm {
             scm {
-                git(‘https://github.com/maheshwar807/sample_job.git’)
+                git{
+		  remote{ url(repo) }
+		  branches('master')
+		}
             }
         }
     }
