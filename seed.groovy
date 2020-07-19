63c0d676c492d2e11e412970e0c2d3b27e48c1b6
maheshwar807 <maheshwar.mannur@gmail.com>
2020-07-19 11:38:48 +0530

    added poll scm

--- seed.groovy
+++ seed.groovy
@@ -1,5 +1,8 @@
 pipelineJob('example') {
 def repo = 'https://github.com/maheshwar807/sample_job.git'
+    triggers{
+     scm('H/1 * * * *')
+    }
     definition {
         cpsScm {
             scm {
